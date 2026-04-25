9660ffcef05aae9474e7f6b3c9ecdddb2806f89c
marc <nguyenvannhv26@gmail.com>
2026-04-25 19:29:21 +0700

    ci: fix base url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL ?? '/',
+  base: '/dot-war-game/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
